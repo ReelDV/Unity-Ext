9621839573b6b9cef81000ccf413819b328ca727
ReelDV <90920356+ReelDV@users.noreply.github.com>
2021-10-07 11:29:57 -0600

    Code build

---
+++ Code Prototype/Assets/Scripts/spawnPoint.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class spawnPoint : MonoBehaviour
+{
+    public GameObject Prefabs;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Instantiate(Prefabs, new Vector3(0, 0, 0), Quaternion.identity);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
