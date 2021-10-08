2ea220a666fad77c1cbfa350514283a4c12365a2
ReelDV <90920356+ReelDV@users.noreply.github.com>
2021-10-08 15:46:50 -0600

    ugh

---
+++ Code Prototype/Assets/Scripts/color_Script.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class color_Script : MonoBehaviour
+{
+    [SerializeField]
+    Renderer m_ObjectRenderer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetComponent<Renderer>().material.color = Random.ColorHSV(01, 1f, 1f, 1f, 0.5f, 1f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
